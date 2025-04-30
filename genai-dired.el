024371a5e5dbb3fee303f9fd68f2dc0d54b78333
Yusuke Watanabe <ywatanabe@alumni.u-tokyo.ac.jp>
2025-04-30 12:27:43 +1000

    Improve file listing in GenAI Dired mode

--- genai-dired.el
+++ genai-dired.el
@@ -1,7 +1,7 @@
 ;;; -*- coding: utf-8; lexical-binding: t -*-
 ;;; Author: ywatanabe
-;;; Timestamp: <2025-04-27 15:46:29>
-;;; File: /home/ywatanabe/.dotfiles/.emacs.d/lisp/genai/genai-dired.el
+;;; Timestamp: <2025-04-28 14:10:17>
+;;; File: /home/ywatanabe/.emacs.d/lisp/genai/genai-dired.el
 
 ;;; Copyright (C) 2025 Yusuke Watanabe (ywatanabe@alumni.u-tokyo.ac.jp)
 
@@ -78,6 +78,61 @@
           (process-file f))
         contents))))
 
+;; (defun genai-on-region-list-files ()
+;;   "List files from dired in a buffer for GenAI prompts."
+;;   (interactive)
+;;   (let* ((marked-files
+;;           (dired-get-marked-files nil nil #'dired-file-marker))
+;;          (white-ext genai-whitelist-extensions)
+;;          (black-ext genai-blacklist-extensions)
+;;          (white-expr genai-whitelist-expressions)
+;;          (black-expr genai-blacklist-expressions)
+;;          (size-limit (* 1024 1024))
+;;          (candidates '())
+;;          (buf (get-buffer-create "*GenAI Files*"))
+;;          (file-count 0))
+;;     (cl-labels ((process-file
+;;                   (f)
+;;                   (cond
+;;                    ((file-directory-p f)
+;;                     (dolist (sub (directory-files f t "^[^.]"))
+;;                       (process-file sub)))
+;;                    ((genai--dired-should-include-file
+;;                      f white-ext black-ext white-expr black-expr
+;;                      size-limit)
+;;                     (push
+;;                      (cons f (file-relative-name f default-directory))
+;;                      candidates)
+;;                     (setq file-count (1+ file-count))))))
+;;       (dolist (f marked-files)
+;;         (process-file f)))
+
+;;     (with-current-buffer buf
+;;       (erase-buffer)
+;;       (let ((region-name (buffer-name (window-buffer))))
+;;         (insert
+;;          (format ";; %d files from %s\n" file-count region-name)))
+
+;;       ;; The order of files awkward
+;;       ;; However, when I change to nreverse, it does not work
+;;       ;; fix here
+;;       ;; (dolist (file-entry (nreverse candidates))
+;;       (dolist (file-entry (reverse candidates))
+;;         (let ((abs-path (car file-entry))
+;;               (rel-path (cdr file-entry)))
+;;           (insert (format "%s\n" rel-path))))
+
+;;       (insert
+;;        "\n;; Instructions:\n"
+;;        ";; 1. Remove any files you don't want to include\n"
+;;        ";; 2. Press 'C-c C-c' when ready to send files to GenAI\n")
+
+;;       (goto-char (point-min))
+;;       (text-mode)
+;;       (set (make-local-variable 'genai--dired-candidates) candidates)
+;;       (local-set-key (kbd "C-c C-c") #'genai--dired-confirm-files))
+;;     (switch-to-buffer-other-window buf)))
+
 (defun genai-on-region-list-files ()
   "List files from dired in a buffer for GenAI prompts."
   (interactive)
@@ -89,23 +144,30 @@
          (black-expr genai-blacklist-expressions)
          (size-limit (* 1024 1024))
          (candidates '())
-         (buf (get-buffer-create "*GenAI Files*"))
+         (buf (get-buffer-create "GenAI Files"))
          (file-count 0))
-    (cl-labels ((process-file
-                  (f)
-                  (cond
-                   ((file-directory-p f)
-                    (dolist (sub (directory-files f t "^[^.]"))
-                      (process-file sub)))
-                   ((genai--dired-should-include-file
-                     f white-ext black-ext white-expr black-expr
-                     size-limit)
-                    (push
-                     (cons f (file-relative-name f default-directory))
-                     candidates)
-                    (setq file-count (1+ file-count))))))
+    (cl-labels
+        ((process-file
+           (f)
+           (cond
+            ;; Recursively process directories
+            ((file-directory-p f)
+             (dolist (sub (directory-files f t "^[^.]"))
+               (process-file sub)))
+            ;; If it meets the file criteria, collect it
+            ((genai--dired-should-include-file
+              f white-ext black-ext white-expr black-expr
+              size-limit)
+             (push (cons f (file-relative-name f default-directory))
+                   candidates)
+             (setq file-count (1+ file-count))))))
+      ;; Process each marked file (recursively if directory)
       (dolist (f marked-files)
         (process-file f)))
+
+    ;; Since we used `push`, the list order is reversed from insertion.
+    ;; To finalize the order, do `nreverse` once before iterating.
+    (setq candidates (nreverse candidates))
 
     (with-current-buffer buf
       (erase-buffer)
@@ -113,8 +175,7 @@
         (insert
          (format ";; %d files from %s\n" file-count region-name)))
 
-      ;; (dolist (file-entry (nreverse candidates))
-      (dolist (file-entry (reverse candidates))
+      (dolist (file-entry candidates)
         (let ((abs-path (car file-entry))
               (rel-path (cdr file-entry)))
           (insert (format "%s\n" rel-path))))
